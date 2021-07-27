9b1e9fa3c4cc5045be70b38d592a9a45e8cb1db9
Diwesh Tanwar <diwesh.tanwar@outlook.com>
2021-07-27 18:51:31 +0530

    CSP Code Unique- on Edit

--- eConnect.Application/Controllers/CSPController.cs
+++ eConnect.Application/Controllers/CSPController.cs
@@ -203,11 +203,16 @@ namespace eConnect.Application.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(UserCSPDetail UserCSPDetail)
         {
+            UserCSPDetailLogic objUserCSPDetailLogic = new UserCSPDetailLogic();
+            var UserCSPDetails = objUserCSPDetailLogic.GetUserCSPDetailByCSPCode(UserCSPDetail.CSPCode).FirstOrDefault();
+            if (UserCSPDetails != null && UserCSPDetails.CSPCode == UserCSPDetail.CSPCode)
+            {
+                ModelState.AddModelError("CSPCode", "CSPCode already exists");
+            }
 
             if (ModelState.IsValid)
             {
-                string fpath = string.Empty;
-                UserCSPDetailLogic objUserCSPDetailLogic = new UserCSPDetailLogic();
+                string fpath = string.Empty;              
                 objUserCSPDetailLogic.UpdateUserCSPDetail(UserCSPDetail);
                 string path = Path.Combine(CSPFilePath, UserCSPDetail.CSPId.ToString());
                 if (UserCSPDetail.PassportSizePhoto != null)
