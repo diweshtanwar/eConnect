d4bfde1bae2a64103d93911ef66cf2cf9a882995
Aditya Biswal <aditya.biswal1046@gmail.com>
2021-09-08 21:03:49 +0530

    Add Logic for absent report

---
+++ eConnect.Logic/AbsentismReportLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Validation;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using eConnect.DataAccess;
+using eConnect.Model;
+
+
+namespace eConnect.Logic
+{
+    public  class AbsentismReportLogic
+    {
+        public IList<tblAbsentismReport> GetAbsentismReportsSearch(string CSP, string Requestedfromdte, string Requestedtodte, string Type, int AbsFrom, int AbsTo, int Ctecount)
+        {
+            using (var unitOfWork = new UnitOfWork(new eConnectAppEntities()))
+            {
+                return unitOfWork.AbsentismReport.GetAbsentismReportsSearch(CSP, Requestedfromdte, Requestedtodte, Type, AbsFrom, AbsTo, Ctecount).ToList();
+            }
+               
+        }
+    }
+}
