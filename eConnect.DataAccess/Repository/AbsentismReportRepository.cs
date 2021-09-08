8c638c600c7f8f80f124cceff81e36c61e97cf8f
Aditya Biswal <aditya.biswal1046@gmail.com>
2021-09-08 21:03:21 +0530

    Add new Repository for absent report

---
+++ eConnect.DataAccess/Repository/AbsentismReportRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eConnect.DataAccess
+{
+    public  class AbsentismReportRepository: Repository<tblAbsentismReport>, IAbsentismReport
+    {
+        public AbsentismReportRepository(eConnectAppEntities appcontext) : base(appcontext)
+        {
+
+        }
+
+        public eConnectAppEntities eConnectAppEntities
+        {
+            get { return new eConnectAppEntities(); }
+        }
+
+        public IList<tblAbsentismReport> GetAbsentismReportsSearch(string CSP, string Requestedfromdte, string Requestedtodte, string Type, int AbsFrom, int AbsTo, int Ctecount)
+        {
+           
+            if (!string.IsNullOrEmpty(CSP) && AbsFrom == 0 && AbsTo == 0 && Ctecount ==0)
+            {
+                eConnectAppEntities.sp_Get_AbsentismReportDetails(CSP, Convert.ToDateTime(Requestedfromdte).ToString("yyyy-MM-dd"), Convert.ToDateTime(Requestedtodte).ToString("yyyy-MM-dd"), AbsFrom, AbsTo, Type, Ctecount, "1");
+            }
+            else if (AbsFrom != 0 && AbsTo != 0 )
+            {
+                
+                eConnectAppEntities.sp_Get_AbsentismReportDetails(CSP, Convert.ToDateTime(Requestedfromdte).ToString("yyyy-MM-dd"), Convert.ToDateTime(Requestedtodte).ToString("yyyy-MM-dd"), AbsFrom, AbsTo, Type, Ctecount, "3");
+            }
+            else if (Ctecount != 0 )
+            {
+                eConnectAppEntities.sp_Get_AbsentismReportDetails(CSP, Convert.ToDateTime(Requestedfromdte).ToString("yyyy-MM-dd"), Convert.ToDateTime(Requestedtodte).ToString("yyyy-MM-dd"), AbsFrom, AbsTo, Type, Ctecount, "4");
+            }
+            else if(!string.IsNullOrEmpty(Requestedfromdte) && !string.IsNullOrEmpty(Requestedtodte))
+            {
+                eConnectAppEntities.sp_Get_AbsentismReportDetails(CSP, Convert.ToDateTime(Requestedfromdte).ToString("yyyy-MM-dd"), Convert.ToDateTime(Requestedtodte).ToString("yyyy-MM-dd"), AbsFrom, AbsTo, Type, Ctecount, "5");
+            }
+            return eConnectAppEntities.tblAbsentismReports.ToList();
+        }
+    }
+}
