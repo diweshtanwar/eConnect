84e9ce4f35ef97f18f10a8f5469970312f3de6d3
Aditya Biswal <aditya.biswal1046@gmail.com>
2021-09-08 21:03:06 +0530

    Add new Repository for absent report

---
+++ eConnect.DataAccess/IRepository/IAbsentismReport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace eConnect.DataAccess
+{
+    public interface IAbsentismReport
+    {
+        IList<tblAbsentismReport> GetAbsentismReportsSearch(string CSP, string Requestedfromdte, string Requestedtodte, string Type, int AbsFrom, int AbsTo, int Ctecount);
+    }
+}
